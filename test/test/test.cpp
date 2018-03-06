f6b38b3a4b28d527699c9af3a187585c00c04416
alexlee1982 <alex_lee1982@hotmail.com>
2018-03-06 08:59:04 +0800

    Revert "Revert "创建工程""
    This reverts commit 08815c63166212877eb1322252e04baa75ccf1ab.

---
+++ test/test/test.cpp
@@ -0,0 +1,78 @@
+// test.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+
+#include "kinematics.h"
+
+KINEMATICS_FORWARD_FLAGS fflags=1;
+KINEMATICS_INVERSE_FLAGS iflags=1;
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	double joint[6];
+	double pre_joint[6];
+	int i = 0;
+	double inv_joint[6];
+	RobotPose world;
+	world.tran.x=0;
+	world.tran.y=0;
+	world.tran.z=0;
+	PmRotationMatrix Ehom;
+	PmHomogeneous hom;
+
+	PmCartesian tran;
+
+
+	//Ehom.x.x=0;
+	//Ehom.x.y=1;
+	//Ehom.x.z=0;
+
+	//Ehom.y.x=0;
+	//Ehom.y.y=0;
+	//Ehom.y.z=1;
+
+	//Ehom.z.x=1;
+	//Ehom.z.y=0;
+	//Ehom.z.z=0;
+
+	//world.tran.x = 0;
+	//world.tran.y = 782.4;
+	//world.tran.z = 0;
+
+	
+
+
+	joint[0] = 80 ;
+	joint[1] = 89 ;
+	joint[2] = 120 ;
+	joint[3] = 120;
+	joint[4] = 120;
+	joint[5] = 120;
+	for (i = 0; i < 6; i++)
+	{
+		pre_joint[i] = joint[i];
+	}
+
+	kinematicsForward(joint, &world, &Ehom, &fflags, &iflags);
+
+	hom.tran = world.tran;
+	hom.rot = Ehom;
+	kinematicsInverse(&world,&hom, inv_joint,pre_joint,1);
+
+	//kinematicsForward_axis5(joint,&world,&Ehom,&fflags,&iflags);
+	//
+	//hom.tran=world.tran;
+	//hom.rot=Ehom;
+	//for (int i = 0; i < 8; )
+	//{
+	//	kinematicsInverse_axis5(&world,&hom,joint, pre_joint,i);
+
+	//	kinematicsForward_axis5(joint,&world,&Ehom,&fflags,&iflags);
+	//	i++;
+	//}
+
+	return 0;
+}
+
